43d79c3efcc823e6cf3a0ee01f2ba10cf2957208
kj432171 <mjmontanob35@gmail.com>
2022-10-14 01:55:09 -0500

    advancing

--- Assets/Scripts/selection_movement/Selection_on_click.cs
+++ Assets/Scripts/selection_movement/Selection_on_click.cs
@@ -4,24 +4,47 @@ using UnityEngine;
 
 public class Selection_on_click : MonoBehaviour
 {
-
+    GameObject[] UnitsToDisable ;
     public bool selected = false;
+
+    void UnselectUnits()
+    {
+        UnitsToDisable = GameObject.FindGameObjectsWithTag("PlayerControlledUnit");
+
+        foreach (GameObject.FindGameObjectWithTag("PlayerControlledUnit") in UnitsToDisable)
+        {
+
+            selected = false;
+
+        }
+
+
+    }
+
+
+
 
 
     void OnMouseDown()
     {
         
 
-        /*Debug.Log("selected");  */ 
+     
         if (selected == false)
         {
+            UnselectUnits();
+
             selected = true;
+            
             Debug.Log("on");
         }
         else if (selected == true)
         {
             selected = false;
+            
             Debug.Log("off");
+
+            
         }
 
     }
